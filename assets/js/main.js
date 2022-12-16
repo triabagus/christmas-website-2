1b9b83fde003e088cb5fbf01d715a1674071900c
triabagus <triatop9@gmail.com>
2022-12-16 14:54:31 +0700

    Parallax Animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,7 +69,7 @@ const scrollActive = () => {
 window.addEventListener('scroll', scrollActive)
 
 /*=============== PARALLAX ===============*/
-
+let parallax = new Rellax('.parallax')
 
 /*=============== GSAP ANIMATION ===============*/
 
