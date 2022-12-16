40622cce2ee9cc3bfd6458fc76c0ecd2ab1ab5c0
triabagus <triatop9@gmail.com>
2022-12-16 15:00:34 +0700

    Gsap animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,6 +72,13 @@ window.addEventListener('scroll', scrollActive)
 let parallax = new Rellax('.parallax')
 
 /*=============== GSAP ANIMATION ===============*/
-
+gsap.from('.home__village', 1.2, {opacity: 0, y: 100, delay: .1})
+gsap.from('.home__pine', 1.2, {opacity: 0, y: 150, delay: .3})
+gsap.from('.home__mountain-2', 1.2, {opacity: 0, y: 150, delay: .5})
+gsap.from('.home__mountain-3', 1.2, {opacity: 0, y: -150, delay: .6})
+gsap.from('.home__mountain-1', 1.2, {opacity: 0, y: 250, delay: .7})
+gsap.from('.home__moon', 1.2, {opacity: 0, y: 200, delay: .8})
+gsap.from('.home__trineo', 1.2, {opacity: 0, y: -200, delay: .8})
+gsap.from('.home__title', 1.2, {opacity: 0, y: -60, delay: 1})
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
